e498e808d450337e7313112b9bb6fbb5464ca432
ARA1DAMT <ramirez21411@iesmarenostrum.com>
2021-06-10 17:01:44 +0200

    Se ha corregido, refactorizado y documentado el código de la clase. Se ha corregido el código de la primera prueba. Se han añadido dos pruebas más.

--- ClassLibrary1/ClassLibrary1/Examen3EV.cs
+++ ClassLibrary1/ClassLibrary1/Examen3EV.cs
@@ -1,72 +1,156 @@
-﻿using System.Collections.Generic;
-
-namespace Examen3EV_NS
+﻿namespace Examen3EV_NS
 {
-    public class estNot  // esta clase nos calcula las estadísticas de un conjunto de notas 
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Clase <see cref="EstadisticasNotasARA2021" /> que se utiliza para calcular las estadisticas de un conjunto de notas.
+    /// </summary>
+    public class EstadisticasNotasARA2021  // esta clase nos calcula las estadísticas de un conjunto de notas 
     {
-        public int sus;  // Suspensos
-        public int apr;  // Aprobados
-        public int not;  // Notables
-        public int sbr;  // Sobresalientes
+        /// <summary>
+        /// Número que corresponde al total de notas que son suspensas.
+        /// </summary>
+        private int suspensos;
 
-        public double media; // Nota media
+        /// <summary>
+        /// Númere que corresponde al total de notas que son aprobados.
+        /// </summary>
+        private int aprobados;
 
-        // Constructor vacío
-        public estNot()
+        /// <summary>
+        /// Número que define al total de notas que son notables.
+        /// </summary>
+        private int notables;
+
+        /// <summary>
+        /// Número que define al total de notas que son sobresalientes.
+        /// </summary>
+        private int sobresalientes;
+
+        /// <summary>
+        /// Número que define a la media del conjunto de notas.
+        /// </summary>
+        private double media;
+
+        /// <summary>
+        /// Obtiene o asigna los Suspensos.
+        /// </summary>
+        public int Suspensos { get => suspensos; set => suspensos = value; }
+
+        /// <summary>
+        /// Obtiene o asigna los Aprobados.
+        /// </summary>
+        public int Aprobados { get => aprobados; set => aprobados = value; }
+
+        /// <summary>
+        /// Obtiene o asigna los Notables.
+        /// </summary>
+        public int Notables { get => notables; set => notables = value; }
+
+        /// <summary>
+        /// Obtiene o asigna los Sobresalientes.
+        /// </summary>
+        public int Sobresalientes { get => sobresalientes; set => sobresalientes = value; }
+
+        /// <summary>
+        /// Obtiene o asigna la Media.
+        /// </summary>
+        public double Media { get => media; set => media = value; }
+
+        
+        /// <summary>
+        /// Inicia una nueva instancia de la clase <see cref="EstadisticasNotasARA2021"/> que está vacía.
+        /// </summary>
+        public EstadisticasNotasARA2021()
         {
-            sus = apr = not = sbr = 0;  // inicializamos las variables
-            media = 0.0;
+            Suspensos = 0; 
+            Aprobados = 0; 
+            Notables = 0; 
+            Sobresalientes = 0;
+            Media = 0.0;
         }
 
         // Constructor a partir de un array, calcula las estadísticas al crear el objeto
-        public estNot(List<int> listnot)
+        /// <summary>
+        /// Inicia una nueva instancia de la clase <see cref="EstadisticasNotasARA2021"/> que recibe una lista.
+        /// </summary>
+        /// <param name="ListaNotas"> Lista que contiene las notas. <see cref="List{int}"/>.</param>
+        public EstadisticasNotasARA2021(List<int> ListaNotas)
         {
-            media = 0.0;
-
-            for (int i = 0; i < listnot.Count; i++)
-            {
-                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
-                else if (listnot[i] > 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
-                else if (listnot[i] > 7 && listnot[i] < 9) not++;// Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
-
-                media = media + listnot[i];
-            }
-
-            media = media / listnot.Count;
+            CalcularEstadisticasARA2021(ListaNotas);
         }
 
+        /// <summary>
+        /// <para>Método que se utiliza para comprobar que los valores de una lista de notas sean correctos y asignar valores a las propiedades de la clase.</para>
+        /// <para> El método calcula:
+        /// <list type="bullet"> 
+        ///     <item> 
+        ///         <description> <see cref="Suspensos"/> </description>
+        ///     </item>
+        ///      <item> 
+        ///         <description> <see cref="Aprobados"/> </description>
+        ///     </item>
+        ///      <item> 
+        ///         <description> <see cref="Notables"/> </description>
+        ///     </item>
+        ///      <item> 
+        ///         <description> <see cref="Sobresalientes"/> </description>
+        ///     </item>
+        ///      <item> 
+        ///         <description> <see cref="Media"/> </description>
+        ///     </item>
+        /// </list> 
+        /// </para>
+        /// </summary>
+        /// <param name="listnot"> Lista de notas de la cual se calculan sus estadisticas. <see cref="List{int}"/>.</param>
+        /// <exception cref="System.ArgumentOutOfRangeException"> Lanza una excepcion si la lista está vacia o si alguna nota no es correcta. </exception>
+        public void CalcularEstadisticasARA2021(List<int> ListNotas)
+        {
+            double media = 0.0;
 
-        // Para un conjunto de listnot, calculamos las estadísticas
-        // calcula la media y el número de suspensos/aprobados/notables/sobresalientes
-        //
-        // El método devuelve -1 si ha habido algún problema, la media en caso contrario	
-        public double calcEst(List<int> listnot)
-        {                                 
-            media = 0.0;
-
-            // TODO: hay que modificar el tratamiento de errores para generar excepciones
-            //
-            if (listnot.Count <= 0)  // Si la lista no contiene elementos, devolvemos un error
-                return -1;
-
-            for (int i=0;i<10;i++)
-                if (listnot[i] < 0 || listnot[i] > 10)      // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
-                return -1;
-
-            for (int i = 0; i < listnot.Count; i++)
+            // Si la lista no contiene elementos, devolvemos un error
+            if (ListNotas.Count <= 0)  
             {
-                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
-                else if (listnot[i] >= 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
-                else if (listnot[i] >= 7 && listnot[i] < 9) not++;// Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
-
-                media = media + listnot[i];
+                throw new ArgumentOutOfRangeException();
             }
 
-            media = media / listnot.Count;
+            // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
+            for (int i = 0; i < ListNotas.Count; i++)
+            {
+                if (ListNotas[i] < 0 || ListNotas[i] > 10)      
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
 
-            return media;
+            for (int i = 0; i < ListNotas.Count; i++)
+            { 
+                // Por debajo de 5 suspenso
+                if (ListNotas[i] < 5)
+                {
+                    Suspensos++;             
+                }
+                // Nota para aprobar: 5
+                else if (ListNotas[i] >= 5 && ListNotas[i] < 7)
+                {
+                    Aprobados++;
+                }
+                // Nota para notable: 7
+                else if (ListNotas[i] >= 7 && ListNotas[i] < 9)
+                {
+                    Notables++;
+                }
+                // Nota para sobresaliente: 9
+                else if (ListNotas[i] >= 9)
+                {
+                    Sobresalientes++;         
+                }
+
+                media = media + ListNotas[i];
+            }
+
+            Media = media / ListNotas.Count;
         }
     }
 }
--- ClassLibrary1/UnitTestProject1/UnitTest1.cs
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Examen3EV_NS;
+using System;
 
 namespace UnitTestProject1
 {
@@ -8,7 +9,7 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PruebaAsignacionCorrectaNotasARA2021()
         {
             List<int> notas = new List<int>();
 
@@ -20,11 +21,46 @@ namespace UnitTestProject1
             notas.Add(4);
             notas.Add(8);
 
+            EstadisticasNotasARA2021 estatsNotas = new EstadisticasNotasARA2021(notas);
+
             double mediaEsperada = 5.143;
-            int susE = 3;
-            int aprE = 1;
-            int notE = 2;
-            int sbrE = 1;
+            int suspensosEsperados = 3;
+            int aprobadosEsperados = 1;
+            int notablesEsperados = 2;
+            int sobresalientesEsperados = 1;
+
+            Assert.AreEqual(mediaEsperada, estatsNotas.Media, 0.001);
+            Assert.AreEqual(suspensosEsperados, estatsNotas.Suspensos);
+            Assert.AreEqual(aprobadosEsperados, estatsNotas.Aprobados);
+            Assert.AreEqual(notablesEsperados, estatsNotas.Notables);
+            Assert.AreEqual(sobresalientesEsperados, estatsNotas.Sobresalientes);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PruebaListaNotasVacias_DebeDarErrorARA2021()
+        {
+            List<int> notas = new List<int>();
+            
+            EstadisticasNotasARA2021 estatsNotas = new EstadisticasNotasARA2021(notas);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PruebaNotaIncorrecta_DebeDarErrorARA2021()
+        {
+            List<int> notas = new List<int>();
+
+            notas.Add(0);
+            notas.Add(-2);
+            notas.Add(9);
+            notas.Add(11);
+
+            EstadisticasNotasARA2021 estatsNotas = new EstadisticasNotasARA2021(notas);
+        }
+
+
+
+
     }
 }
